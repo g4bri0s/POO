8bb92a62aa0fba6c2d33878eb3b22f88c59a6cbc
g4bri0s <g.cristianods@gmail.com>
2024-03-28 10:00:58 -0300

    correção de exec 1

--- poo/src/main/java/com/example/exec1/model/Circulo.java
+++ poo/src/main/java/com/example/exec1/model/Circulo.java
@@ -4,11 +4,15 @@ public class Circulo {
     private double raio;
 
     public Circulo() {
-    
+
     }
 
     public Circulo(double raio) {
         this.raio = raio;
+    }
+
+    public double getRaio() {
+        return raio;
     }
 
     public double getArea() {
--- poo/src/main/java/com/example/exec1/view/view.java
+++ poo/src/main/java/com/example/exec1/view/view.java
@@ -9,31 +9,34 @@ import com.example.exec1.model.Circulo;
 
 public class view {
 
-    static Pessoa pessoa = new Pessoa("João", LocalDate.of(2000, 3, 18), "Estudante");
-    static Funcionario funcionario = new Funcionario("Maria", 2000, "Gerente");
-    static ContaBancaria conta = new ContaBancaria(123, "Maria");
-    static Produto produto = new Produto("Arroz", 5f, 10);
-    static Circulo circulo = new Circulo(5d);
+        static Pessoa pessoa = new Pessoa("João", LocalDate.of(2000, 3, 18), "Estudante");
+        static Funcionario funcionario = new Funcionario("Maria", 2000, "Gerente");
+        static ContaBancaria conta = new ContaBancaria(123, "Maria");
+        static Produto produto = new Produto("Arroz", 5f, 10);
+        static Circulo circulo = new Circulo(5d);
 
-    public static void main(String[] args) {
-        System.out.println("Nome: " + pessoa.getNome() + "\nData de Nascimento: " + pessoa.getDataNascimento()
-                + "\nProfissão: " + pessoa.getProfissao() + "\nIdade: " + pessoa.getIdade());
+        public static void main(String[] args) {
+                System.out.println("Nome: " + pessoa.getNome() + "\nData de Nascimento: " + pessoa.getDataNascimento()
+                                + "\nProfissão: " + pessoa.getProfissao() + "\nIdade: " + pessoa.getIdade());
 
-        System.out.println("Nome: " + funcionario.getNome() + "\nSalário: " + funcionario.getSalario() + "\nCargo: "
-                + funcionario.getCargo());
+                System.out.println("\nNome: " + funcionario.getNome() + "\nSalário: " + funcionario.getSalario()
+                                + "\nCargo: "
+                                + funcionario.getCargo());
 
-        System.out.println(
-                "Número da Conta: " + conta.getNumeroConta() + "\nSaldo: " + conta.getSaldo());
+                System.out.println(
+                                "\nNúmero da Conta: " + conta.getNumeroConta() + "\nSaldo: " + conta.getSaldo());
 
-        conta.realizarDeposito(1000);
-        System.out.println("Saldo após depósito: " + conta.getSaldo());
-        System.out.println("Saque de 500: " + conta.realizarSaque(500));
-        System.out.println("Saldo após saque: " + conta.getSaldo());
+                conta.realizarDeposito(1000);
+                System.out.println("Saldo após depósito: " + conta.getSaldo());
+                System.out.println("Saque de: " + conta.realizarSaque(500));
+                System.out.println("Saldo após saque: " + conta.getSaldo());
 
-        System.out.println("\nÁrea: " + circulo.getArea() + "\nPerímetro: " + circulo.getPerimetro());
+                System.out.println("\nRaio do circulo:" + circulo.getRaio() + "\nÁrea: " + circulo.getArea()
+                                + "\nPerímetro: " + circulo.getPerimetro());
 
-        System.out.println("\nProduto: " + produto.getNome() + "\nPreço: " + produto.getPreco() + "\nQuantidade: "
-                + produto.getQuantidade() + "\nTotal: " + produto.totalValorEstoque());
-    }
+                System.out.println(
+                                "\nProduto: " + produto.getNome() + "\nPreço: " + produto.getPreco() + "\nQuantidade: "
+                                                + produto.getQuantidade() + "\nTotal: " + produto.totalValorEstoque());
+        }
 
 }
